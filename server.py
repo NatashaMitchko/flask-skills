b7930ad0aad5c8a676dbc661bfaedf6eddd58b8a
Natasha <natasha.mitchko@gmail.com>
2017-04-23 21:31:38 +0000

    initial files

---
+++ server.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from jinja2 import StrictUndefined
+
+
+app = Flask(__name__)
+app.jinja_env.undefined = StrictUndefined
+app.jinja_env.auto_reload = True
+
+# Required to use Flask sessions and the debug toolbar
+app.secret_key = "ABC"
+
+
+# YOUR ROUTES GO HERE
+
+
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the
+    # point that we invoke the DebugToolbarExtension
+    app.debug = True
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+    app.run(host="0.0.0.0")
